c79af74a7ae4e5e18a4edb8f75acd1b7d5690247
Vitaliy <vitaliyufc@gmail.com>
2023-11-28 06:09:39 +0300

    Task completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Machine[] machines = {new Truck(), new Bulldozer(), new Excavator()};
+
+        for (Machine car: machines)
+        {
+            car.doWork();
+            car.stopWork();
+            System.out.println("\n");
+        }
     }
 }
